a61310944caf6217ec9002959c8ce7a5f4743eb9
Klasnov <2654403080@qq.com>
2022-08-19 00:00:07 +0800

    Web Development

---
+++ js/upload.js
@@ -0,0 +1,15 @@
+function toSngl() {
+	window.location.href = "single_upload.html";
+}
+
+function toMulti() {
+	window.location.href = "multi_upload.html";
+}
+
+function toZip() {
+	window.location.href = "zip_upload.html";
+}
+
+function toPosResult() {
+	window.location.href = "result_positive.html";
+}
